374de306f915bb26b1b31c95fd49827bdb4475af
BenTenmann <benji.tenmann@me.com>
2022-01-02 16:14:22 +0000

    feat: add source tar ball

--- setup.py
+++ setup.py
@@ -3,11 +3,37 @@ import os
 import platform
 import shutil
 import subprocess
+import tempfile
+from pathlib import Path
 
 from setuptools import setup, Extension
 
+DIR = Path(__file__).parent
+
 OPENMP_DISABLED = os.environ.get('OPENMP_DISABLED', False)
 libraries = ['clustalo', 'stdc++']
+
+
+def download_argtable(system):
+    commands = {
+        'Darwin': [['brew', 'install', 'argtable']],
+        'Linux': [['sudo', 'apt', 'update'], ['sudo', 'apt', 'install', 'libargtable2-dev']]
+    }.get(system, [[]])
+
+    for cmd in commands:
+        subprocess.run(cmd, check=True)
+
+
+def build_clustal(system):
+    download_argtable(system)
+    tmp_dir = tempfile.mkdtemp()
+
+    tar_ball = 'clustal-omega-1.2.4.tar.gz'
+    wd = Path(tmp_dir)
+    shutil.unpack_archive(DIR / tar_ball, wd)
+
+    subprocess.run('./configure --with-pic --with-openmp && make && sudo make install',
+                   cwd=wd / 'clustal-omega-1.2.4', shell=True, check=True)
 
 
 class BuildFlags:
@@ -27,10 +53,9 @@ class BuildFlags:
     compiler: list
     linker: list
 
-    def __init__(self):
-        self._system = platform.system()
-        tool, formula = self._tools_formulae.get(self._system, ('apt', 'libomp-dev'))
-        args = self._args.get(self._system, self._default_args)
+    def __init__(self, system):
+        tool, formula = self._tools_formulae.get(system, ('apt', 'libomp-dev'))
+        args = self._args.get(system, self._default_args)
 
         not_found = self._libomp_check(tool, formula)
         if not_found is not None:
@@ -53,19 +78,27 @@ class BuildFlags:
         return None
 
 
-flags = BuildFlags()
-module = Extension('clustalo',
-                   sources=['clustalo.c'],
-                   include_dirs=['/usr/include/clustalo', '/usr/local/include/clustalo'],
-                   library_dirs=['/usr/local/lib'],
-                   libraries=libraries,
-                   extra_compile_args=flags.compiler,
-                   extra_link_args=flags.linker)
+def main():
+    system = platform.system()
 
-setup(name='clustalo',
-      version='0.1.2',
-      description='Python wrapper around libclustalo',
-      author='Benchling Engineering',
-      author_email='eng@benchling.com',
-      url='https://github.com/benchling/clustalo-python',
-      ext_modules=[module])
+    build_clustal(system)
+    flags = BuildFlags(system)
+    module = Extension('clustalo',
+                       sources=['clustalo.c'],
+                       include_dirs=['/usr/include/clustalo', '/usr/local/include/clustalo'],
+                       library_dirs=['/usr/local/lib'],
+                       libraries=libraries,
+                       extra_compile_args=flags.compiler,
+                       extra_link_args=flags.linker)
+
+    setup(name='clustalo',
+          version='0.1.2',
+          description='Python wrapper around libclustalo',
+          author='Benchling Engineering',
+          author_email='eng@benchling.com',
+          url='https://github.com/benchling/clustalo-python',
+          ext_modules=[module])
+
+
+if __name__ == '__main__':
+    main()
